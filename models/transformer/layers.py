406c3b1d2473a7bd2abf2af7e0bfcb90938af68f
Romain <romain.thoreau@agroparistech.fr>
2025-11-13 17:35:10 +0100

    first commit

---
+++ models/transformer/layers.py
@@ -0,0 +1,81 @@
+''' Define the sublayers in encoder/decoder layer 
+Source: https://github.com/jadore801120/attention-is-all-you-need-pytorch/
+'''
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class EmbeddingLayer(nn.Module):
+    '''TODO: compute embeddings from raw pixel set data.
+    '''
+
+    def __init__(self):
+        super(EmbeddingLayer, self).__init__()
+
+    def forward(self, x):
+        raise NotImplementedError
+    
+
+class SpectralIndicesLayer(nn.Module):
+    '''TODO: compute features based on NDVI and BI time series from raw pixel set data:
+        - NDVI = (NIR - RED) / (NIR + RED)
+        - BI = ((SWIR1 + RED) - (NIR + BLUE)) / ((SWIR1 + RED) + (NIR + BLUE))
+    Concatenate pixel-wise spectral indices and pass output into a linear layer and 
+    a normalization layer.
+    '''
+
+    def __init__(self):
+        super(SpectralIndicesLayer, self).__init__()
+        
+
+    def forward(self):
+        raise NotImplementedError
+    
+
+class PositionalEncoding(nn.Module):
+    ''' Positional Encoding Layer.
+    Source: https://github.com/jadore801120/attention-is-all-you-need-pytorch/blob/master/transformer/Models.py
+    TODO: Update the positional encoding as described in "Satellite Image Time Series 
+    Classification with Pixel-Set Encoders and Temporal Self-Attention, Garnot et al."
+    '''
+    def __init__(self, d_hid, n_position=200):
+        super(PositionalEncoding, self).__init__()
+
+        # Not a parameter
+        self.register_buffer('pos_table', self._get_sinusoid_encoding_table(n_position, d_hid))
+
+    def _get_sinusoid_encoding_table(self, n_position, d_hid):
+        ''' Sinusoid position encoding table '''
+        # TODO: make it with torch instead of numpy
+
+        def get_position_angle_vec(position):
+            return [position / np.power(10000, 2 * (hid_j // 2) / d_hid) for hid_j in range(d_hid)]
+
+        sinusoid_table = np.array([get_position_angle_vec(pos_i) for pos_i in range(n_position)])
+        sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
+        sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
+
+        return torch.FloatTensor(sinusoid_table).unsqueeze(0)
+
+    def forward(self, x):
+        return x + self.pos_table[:, :x.size(1)].clone().detach()
+    
+
+class Temporal_Aggregator(nn.Module):
+    ''' TODO: aggregate embeddings that are not masked.
+    '''
+    def __init__(self, mode='mean'):
+        super(Temporal_Aggregator, self).__init__()
+        self.mode = mode
+
+    def forward(self, data, mask):
+        if self.mode == 'mean':
+            raise NotImplementedError
+        elif self.mode == 'identity':
+            out = data
+        else:
+            raise NotImplementedError
+        return out
---
+++ models/transformer/transformer.py
@@ -0,0 +1,145 @@
+''' Transformer model 
+Credits: https://github.com/jadore801120/attention-is-all-you-need-pytorch/
+'''
+import torch
+import torch.nn as nn
+
+from models.transformer.layers import PositionalEncoding,\
+    Temporal_Aggregator, EmbeddingLayer, SpectralIndicesLayer
+
+from models.transformer.attention import MultiHeadAttention,\
+    LearnableQueryMultiHeadAttention, PositionwiseFeedForward
+
+
+class Transformer(nn.Module):
+    ''' A sequence to sequence model with attention mechanism. '''
+
+    def __init__(
+            self, 
+            n_channels, 
+            n_pixels,
+            d_model=512, 
+            d_inner=2048,
+            n_layers=6, 
+            n_head=8, 
+            d_k=64, 
+            d_v=64, 
+            dropout=0.1, 
+            pad_value=0.,
+            scale_emb_or_prj='prj', 
+            n_position=365, 
+            T=1000, 
+            return_attns=False, 
+            learnable_query=False, 
+            spectral_indices_embedding=False,
+            channels={"blue": 1, "red": 3, "near_infrared": 6, "swir1": 8}, 
+            compute_values=True):
+
+        super().__init__()
+
+        # In section 3.4 of paper "Attention Is All You Need", there is such detail:
+        # "In our model, we share the same weight matrix between the two
+        # embedding layers and the pre-softmax linear transformation...
+        # In the embedding layers, we multiply those weights by \sqrt{d_model}".
+        #
+        # Options here:
+        #   'emb': multiply \sqrt{d_model} to embedding output
+        #   'prj': multiply (\sqrt{d_model} ^ -1) to linear projection output
+        #   'none': no multiplication
+
+        assert scale_emb_or_prj in ['emb', 'prj', 'none']
+
+        self.scale_emb = scale_emb_or_prj == 'emb'
+        self.scale_proj = scale_emb_or_prj == 'prj'
+        self.d_model = d_model
+        self.n_head = n_head
+        self.pad_value = pad_value
+        self.return_attns = return_attns
+        self.learnable_query = learnable_query
+        
+        if spectral_indices_embedding:
+            self.embedding = SpectralIndicesLayer(d_model, **channels)
+        else:
+            self.embedding = EmbeddingLayer(n_channels, n_pixels, d_model)
+
+        if learnable_query:
+            self.layer_stack = nn.ModuleList([
+                EncoderLayer(d_model, d_inner, n_head, d_k, d_v, 
+                             dropout=dropout, learnable_query=True, compute_values=compute_values)
+                             ])
+            self.temporal_aggregator = Temporal_Aggregator(mode='identity')
+        else:
+            self.layer_stack = nn.ModuleList([
+                EncoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout, learnable_query=False)
+                for _ in range(n_layers)])
+            self.temporal_aggregator = Temporal_Aggregator(mode='mean')
+        
+        self.position_enc = PositionalEncoding(d_model, n_position=n_position, T=T)
+        self.dropout = nn.Dropout(p=dropout)
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+       
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p) 
+
+    def encoder(self, x, doys, mask):
+        enc_slf_attn_list = []
+
+        # -- Forward
+        if self.scale_emb:
+            enc_output = x * self.d_model ** 0.5
+        else:
+            enc_output = x
+
+        device = x.device
+        pos_embedding = self.position_enc(doys).to(device)
+        enc_output = self.dropout(enc_output + pos_embedding)
+        enc_output = self.layer_norm(enc_output)
+
+        for enc_layer in self.layer_stack:
+            enc_output, enc_slf_attn = enc_layer(enc_output, mask)
+            enc_slf_attn_list += [enc_slf_attn] if self.return_attns else []
+
+        if self.return_attns:
+            return enc_output, enc_slf_attn_list
+        return enc_output
+       
+
+    def forward(self, data, doys=None):
+        '''TODO: implement the forward pass.
+        '''
+        if doys is None:
+            doys = torch.zeros((data.shape[0], data.shape[1]))
+
+        embeddings = ...
+        temporal_mask = ...
+        
+        if self.return_attns:
+            enc_output, attns = self.encoder(embeddings, doys, temporal_mask)
+        else:
+            enc_output = self.encoder(embeddings, doys, temporal_mask)
+            attns = None
+
+        enc_output = ...
+
+        return enc_output, attns
+    
+
+class EncoderLayer(nn.Module):
+    ''' Compose with two layers
+    Source: https://github.com/jadore801120/attention-is-all-you-need-pytorch/blob/master/transformer/Layers.py 
+    '''
+
+    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1, learnable_query=False, compute_values=True):
+        super(EncoderLayer, self).__init__()
+        if learnable_query:
+            self.slf_attn = LearnableQueryMultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout, compute_values=compute_values)
+        else:
+            self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
+
+    def forward(self, enc_input, slf_attn_mask=None):
+        enc_output, enc_slf_attn = self.slf_attn(
+            enc_input, mask=slf_attn_mask)
+        enc_output = self.pos_ffn(enc_output)
+        return enc_output, enc_slf_attn
